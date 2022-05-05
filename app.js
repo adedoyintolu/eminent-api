b89e5f220df5bf1e5087941281bb70ddd091943c
MartianTolu <tolulope.adedoyin@deimos.co.za>
2022-05-05 08:15:59 +0100

    Refined the CRUD routes

--- app.js
+++ app.js
@@ -1,93 +1,31 @@
-const Joi = require('joi')
 const express = require("express");
-const func = require('joi/lib/types/func');
 const app = express();
+const programmingLanguagesRouter = require("./routes/programmingLanguages");
+
 
 app.use(express.json());
 
-const courses = [
-  { id: 1, name: 'Python' },
-  { id: 2, name: 'Javascript' },
-  { id: 3, name: 'Node' },
-  { id: 4, name: "Java" }
-]
-
-function validateCourse(course) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required()
-  });
-
-  return Joi.validate(course, schema);
-}
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Ok');
 });
 
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
+app.use("/programming-languages", programmingLanguagesRouter);
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+  return;
 });
 
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) {
-    return res.status(404).send('Course not found');
-  } else {
-    res.send(course);
-  }
-});
 
-app.post('/api/courses', (req, res) => {
-
-  const { error } = validateCourse(req.body);
-  if (error) {
-    // 400 Bad Request
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course)
-})
-
-app.put('/api/courses/:id', (req, res) => {
-  // Look up the course
-  // If not existing, return 404
-
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) {
-    return res.status(404).send('Course not found');
-  }
-  // Validate
-  // If invalid, return 400 - Bad request
-
-  // const result = validateCourse(req.body);
-  const { error } = validateCourse(req.body);
-  if (error) {
-    // 400 Bad Request
-    return res.status(400).send(error.details[0].message);
-  }
-  // Update course
-  course.name = req.body.name
-  //Return the updated course
-  res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) {
-    return res.status(404).send('Course not found');
-  }
-
-  //Delete
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: "db4free.net",
+    user: "eminent",
+    password: "doyin1929",
+    database: "eminentdb",
+  },
+  listPerPage: 10,
+};
+module.exports = config;
